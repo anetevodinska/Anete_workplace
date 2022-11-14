124f1207ad0893a7fd3e5d640d9b72ae88911c74
Anete Vodinska <anetekonusa@gmail.com>
2022-11-14 21:14:21 +0200

    parctical 14/11

---
+++ Practical14-11/Practical14-11/Program.cs
@@ -0,0 +1,21 @@
+﻿class Practical
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please, type number:");
+        string numb1 = Console.ReadLine();
+        Console.WriteLine("Please, enter one more number");
+        string numb2 = Console.ReadLine();
+        int number1 = Int32.Parse(numb1);
+        int number2 = Int32.Parse(numb2);
+        int result = number1 + number2;
+        int result1 = number1 - number2;
+        int result2 = number1 * number2;
+        int result3 = number1 / number2;
+        Console.WriteLine(result);
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+        Console.WriteLine(result3);
+    }
+}
+
